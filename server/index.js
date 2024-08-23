e0729737bc23aa6dc876b1c29f8a553892eeb4af
Reyvi Rahman <reyvirahman@gmail.com>
2024-08-23 17:44:10 +0700

    Adding Post page

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const db = require('./models');
+
+// Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {

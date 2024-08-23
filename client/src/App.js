3759c8ffcd22107fc3cc16e4397d62e75f9a4c47
Reyvi Rahman <reyvirahman@gmail.com>
2024-08-23 21:21:06 +0700

    Adding bootstrap and styling landingpage with bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import './App.css';
+// import './App.css';
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Card from 'react-bootstrap/Card';
 
 function App() {
   const [listOfPosts, setListOfPosts] = useState([]);
@@ -12,19 +13,20 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="d-flex flex-column justify-content-center align-items-center">
       {listOfPosts.map((value, key) => {
         return (
-          <div className='post'>
-            <div className='title'>{value.title}</div>
-            <div className='body'>
-              {value.postText}
-            </div>
-            <div className='footer'>
-              {value.userName}
-            </div>
-            
-          </div>
+          <Card style={{ width: '500px' }} className="text-center mt-4">
+            <Card.Header className="bg-primary text-white py-3">
+              <Card.Title>{value.title}</Card.Title>
+            </Card.Header>
+            <Card.Body className="d-flex align-items-center justify-content-center" style={{height: '180px'}}>
+              <Card.Text>
+                {value.postText}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer className="bg-primary text-white py-3 text-start">{value.username}</Card.Footer>
+          </Card>            
         );
       })}
     </div>
